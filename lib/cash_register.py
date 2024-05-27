ff9618d3b1f078e5a47287ed32e7acd2a54da34d
Gideon Macharia <kamaugedion95@gmail.com>
2024-05-28 00:08:40 +0300

    initial commmit

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+    def __init__(self, discount=0):
+        self.discount = discount
+        self.total = 0
+        self.items = []
+
+    def add_item(self,title,price,quantity=1):
+        for _ in range(quantity):
+            self.items.append(title)
+            self.total += price
+
